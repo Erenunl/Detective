ac4b0ea248d39b8bd3797734be6e8cb97ca4c91d
TheOkanA <okan.akcicek@bahcesehir.edu.tr>
2024-05-31 03:03:22 +0300

    Dialogue
    Diyalog sistem tam bir şekilde kayıt alındı kontrol edilsin eksik gedik bir şey olursa tekrar duruma bağlı puslarım

---
+++ Assets/Scripts/Dialogue/Scripts/AdvanceDialogueManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdvanceDialogueManager : MonoBehaviour
+{
+    private AdvancedDialogueSO currentConversation;
+    private int stepNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void InitiateDialogue(NPCDialogue nPCDialogue)
+    {
+
+    }
+
+    
+}
+public enum DialogueActors
+{
+    Willard,
+    Martin,
+    Leila,
+    Branch,
+};
